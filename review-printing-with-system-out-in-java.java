a49fa6b0f15bca1d6831d5e660166d4d64ea6020
Beregszászi István <beistvan@gmail.com>
2024-06-30 10:55:59 +0200

    Review Printing With System Out In Java

---
+++ review-printing-with-system-out-in-java.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Introduction {
+  public static void main(String[] args) {
+    String name = "John";
+    int age = 25;
+    String hobbies = "swimming and hiking";
+
+    System.out.println("My name is " + name);
+    System.out.printf("I am %d years old\n", age);
+    System.out.print("My hobbies are " + hobbies);
+  }
+}
+
