f316467a54122923569fde5057c9ebb0a2562c27
Beregszászi István <beistvan@gmail.com>
2024-07-09 16:32:08 +0200

    Generic Classes - Class Container

---
+++ generic-classes-class-container.java
@@ -0,0 +1,19 @@
+public class Container <T> {
+  private T data;
+
+  public Container(T data) {
+    this.data = data;
+  }
+
+  public void setData(T data) {
+    this.data = data;
+  }
+
+  public T getData() {
+    return this.data;
+  }
+
+  public static void main(String[] args) {
+    
+  }
+}
