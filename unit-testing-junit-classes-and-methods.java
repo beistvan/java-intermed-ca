8db4519c44f05722f27659149236c434240ae9d0
Beregszászi István <beistvan@gmail.com>
2024-08-26 02:02:52 +0200

    Unit Testing JUnit Classes And Methods In Java - Midification

--- unit-testing-junit-classes-and-methods.java
+++ unit-testing-junit-classes-and-methods.java
@@ -30,4 +30,8 @@ public class JUnitExample {
     a = 0;
   }
 }
-
+/* Run tests
+$ export JUNIT4_CLASSPATH="/usr/share/java/junit4-4.12.jar:/usr/share/java/hamcrest-core-1.3.jar"
+$ javac -cp .:$JUNIT4_CLASSPATH ./*.java
+$ java -cp .:$JUNIT4_CLASSPATH org.junit.runner.JUnitCore JUnitExample
+*/
