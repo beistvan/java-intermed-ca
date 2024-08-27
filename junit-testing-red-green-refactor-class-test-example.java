bd2a855ea324969f3489a61851d94969f11069e5
Beregszászi István <beistvan@gmail.com>
2024-08-28 00:15:30 +0200

    JUnit Testing - Red-Green-Refactor - Class TestExample - Run

--- junit-testing-red-green-refactor-class-test-example.java
+++ junit-testing-red-green-refactor-class-test-example.java
@@ -18,3 +18,8 @@ public class TestExample {
     assertThat(ex.isPositive(c), equalTo(-1));
   }
 }
+/* Run tests:
+export JUNIT4_CLASSPATH="/usr/share/java/junit4-4.12.jar:/usr/share/java/hamcrest-core-1.3.jar"
+javac -cp .:$JUNIT4_CLASSPATH ./*.java
+java -cp .:$JUNIT4_CLASSPATH org.junit.runner.JUnitCore TestExample
+*/
