67850232b314e15ba654e55d68a4738315e754eb
Beregszászi István <beistvan@gmail.com>
2024-09-18 09:29:41 +0200

    JUnit Testing - @Test Methods And Assets - Class TestExample

--- junit-testing-test-methods-and-assets-class-test-example.java
+++ junit-testing-test-methods-and-assets-class-test-example.java
@@ -15,3 +15,7 @@ public class TestExample {
     assertThat(ex.factorial(0), equalTo(1));
   }
 }
+// Complie and run
+//export JUNIT4_CLASSPATH="/usr/share/java/junit4-4.12.jar:/usr/share/java/hamcrest-core-1.3.jar"
+//javac -cp .:$JUNIT4_CLASSPATH ./*.java
+//java -cp .:$JUNIT4_CLASSPATH org.junit.runner.JUnitCore TestExample
