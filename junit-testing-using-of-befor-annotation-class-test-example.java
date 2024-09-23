74eaab06517fb15abf2d5a51928ff9a7b3a0300b
Beregszászi István <beistvan@gmail.com>
2024-09-23 22:12:42 +0200

    JUnit Testing - Using Of @Before Annotation - Class TestExample - Run Script

--- junit-testing-using-of-befor-annotation-class-test-example.java
+++ junit-testing-using-of-befor-annotation-class-test-example.java
@@ -30,3 +30,9 @@ public class TestExample {
     assertThat(a, equalTo(10));
   }
 }
+
+/* Run:
+export JUNIT4_CLASSPATH="/usr/share/java/junit4-4.12.jar:/usr/share/java/hamcrest-core-1.3.jar"
+javac -cp .:$JUNIT4_CLASSPATH ./*.java
+java -cp .:$JUNIT4_CLASSPATH org.junit.runner.JUnitCore TestExample
+ */
